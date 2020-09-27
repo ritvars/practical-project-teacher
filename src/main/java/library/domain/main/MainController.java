0ce06e7acb491d98a66da47e7d58c156e4043b24
Ritvars Dortans <ritvars.dortans@neotech.lv>
2020-09-27 13:12:40 +0300

    Remove redundant properties from main controller

--- src/main/java/library/domain/main/MainController.java
+++ src/main/java/library/domain/main/MainController.java
@@ -1,17 +1,23 @@
 package library.domain.main;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
-public class MainController {
-
-    @FXML private AnchorPane navigation;
+public class MainController implements Initializable {
 
     @FXML private BorderPane content;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Main controller initialized!");
+    }
 
     @FXML
     public void switchPanel(ActionEvent event) throws Exception {
